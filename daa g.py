833ed6c2d92e5072966ed0afc9553947417d29d1
aman7081 <113586030+aman7081@users.noreply.github.com>
2022-09-15 13:29:36 +0530

    Add files via upload

---
+++ daa g.py
@@ -0,0 +1,9 @@
+lst = []
+num = int(input("enter the size of array: "))
+print("enter the array elements: ")
+for n in range(num):
+    numbers=int(input())
+    lst.append(numbers)
+    print("max:",max(lst))
+         
+         
